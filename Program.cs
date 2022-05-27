58af7ca9d8261eb23f418ae53046120a7c48b5ee
Sara Simoni <78156114+sarasimoni@users.noreply.github.com>
2022-05-27 12:21:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIO.Abstracao.RPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Wizard.cs
@@ -0,0 +1,17 @@
+namespace DIO.Abstracao.RPG.src.Entities
+{
+    public class Wizard : Hero
+    {
+        public Wizard(string name, int level, string heroType)
+        {
+            Name = name;
+            Level = level;
+            HeroType = heroType;
+        }
+
+        public override string Attack()
+        {
+            return Name + " Atacaou com sua magia!!";
+        }
+    }
+}
